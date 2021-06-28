956b00eda66b08162d3d58685109bac90170ffa4
David Spruill <david@spruill.email>
2021-06-27 17:07:56 -0700

    test

--- GetImage.py
+++ GetImage.py
@@ -17,12 +17,15 @@ while rval:
     rval, frame = vc.read()
     key = cv2.waitKey(15)
 
-    smaller = cv2.resize(frame, (16,9), interpolation=cv2.INTER_CUBIC)
-    smaller = cv2.blur(smaller, (2,2), borderType=cv2.BORDER_REFLECT)
+    #smaller = cv2.resize(frame, (16,9), interpolation=cv2.INTER_CUBIC)
+    #smaller = cv2.blur(smaller, (2,2), borderType=cv2.BORDER_REFLECT)
 
     # print(smaller[1][1], smaller[1][2], smaller[1][3])
 
-    larger = cv2.resize(smaller, (640, 480), interpolation=cv2.INTER_AREA)
+    #larger = cv2.resize(smaller, (640, 480), interpolation=cv2.INTER_AREA)
+
+
+    larger = cv2.GaussianBlur(frame, (10,10))
 
     cv2.rectangle(larger, (10,10), (630, 470), (0,0,0), -1)
 
