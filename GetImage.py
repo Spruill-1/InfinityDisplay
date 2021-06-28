168ad88b08f25117c7839960a8c3600114f6e02a
David Spruill <david@spruill.email>
2021-06-28 00:05:30 -0700

    clean up the math for speed optimizations

--- GetImage.py
+++ GetImage.py
@@ -1,37 +1,73 @@
-from typing import ItemsView
 import cv2
-cv2.namedWindow("preview")
+import numpy as np
+from operator import floordiv
+
 vc = cv2.VideoCapture(0)
+cv2.namedWindow("top")
+cv2.namedWindow("bottom")
 
 if vc.isOpened():
     rval, frame = vc.read()
-    # cv2.imshow("preview",frame)
 
     height = len(frame)
     width = len(frame[0])
-
 else:
     rval = False
 
+lights = (16,9)
+history = 5
+
+top    = np.zeros((lights[0],   1, 3), dtype = "uint8")
+bottom = np.zeros((lights[0],   1, 3), dtype = "uint8")
+left   = np.zeros((lights[1]-2, 1, 3), dtype = "uint8")
+right  = np.zeros((lights[1]-2, 1, 3), dtype = "uint8")
+
+topHistory    = np.zeros((lights[0],   history, 3), dtype = "uint8")
+bottomHistory = np.zeros((lights[0],   history, 3), dtype = "uint8")
+leftHistory   = np.zeros((lights[1]-2, history, 3), dtype = "uint8")
+rightHistory  = np.zeros((lights[1]-2, history, 3), dtype = "uint8")
+
+currIndex = 0
+prevIndex = history - 1
+
 while rval:
     rval, frame = vc.read()
+    smaller = cv2.resize(frame, lights)
+
+    for x in range(0,lights[0]-1):
+        top[x][0]    = top[x][0]    - topHistory[x][currIndex]
+        bottom[x][0] = bottom[x][0] - bottomHistory[x][currIndex]
+
+        topHistory[x][currIndex]    = smaller[0][x] / history
+        bottomHistory[x][currIndex] = smaller[lights[1]-1][x] / history
+
+        top[x][0]    = top[x][0]    + topHistory[x][currIndex]
+        bottom[x][0] = bottom[x][0] + bottomHistory[x][currIndex]
+
+    for y in range(1,lights[1]-2):
+        left[y][0]  = left[y][0]  - leftHistory[y][currIndex]
+        right[y][0] = right[y][0] - rightHistory[y][currIndex]
+        
+        leftHistory[y][currIndex]  = smaller[y][0] / history
+        rightHistory[y][currIndex] = smaller[y][lights[0]-1] / history
+
+        left[y][0]  = left[y][0]  + leftHistory[y][currIndex]
+        right[y][0] = right[y][0] + rightHistory[y][currIndex]
+
+    prevIndex = currIndex
+    currIndex = currIndex + 1
+    if currIndex >= history:
+        currIndex = 0
+
+    topLarger = cv2.resize(cv2.transpose(top), (width, int(height/lights[1])), interpolation=cv2.INTER_AREA)
+    cv2.imshow("top",topLarger)
+    
+    bottomLarger = cv2.resize(cv2.transpose(bottom), (width, int(height/lights[1])), interpolation=cv2.INTER_AREA)
+    cv2.imshow("bottom",bottomLarger)
+
+    
     key = cv2.waitKey(15)
-
-    #smaller = cv2.resize(frame, (16,9), interpolation=cv2.INTER_CUBIC)
-    #smaller = cv2.blur(smaller, (2,2), borderType=cv2.BORDER_REFLECT)
-
-    # print(smaller[1][1], smaller[1][2], smaller[1][3])
-
-    #larger = cv2.resize(smaller, (640, 480), interpolation=cv2.INTER_AREA)
-
-
-    larger = cv2.GaussianBlur(frame, (11,11), 0)
-
-    cv2.rectangle(larger, (11,11), (630, 470), (0,0,0), -1)
-
-    cv2.imshow("preview",larger)
-
     if key == 27: # ESC key
         break
 
-cv2.destroyWindow("preview")
+cv2.destroyAllWindows()
